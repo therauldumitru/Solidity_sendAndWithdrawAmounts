2420c3f18c39e21f277791a56d84d368a7ddb3e2
therauldumitru <97385027+therauldumitru@users.noreply.github.com>
2023-06-06 22:29:54 +0300

    WIthdrawAmounts.sol

---
+++ WIthdrawAmounts.sol
@@ -0,0 +1,28 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.14;
+
+contract WithdrawAmounts {
+
+    mapping(address => uint) public balanceReceived;
+
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+    function sendMoney() public payable {
+        balanceReceived[msg.sender] += msg.value;
+    }
+
+    function withdrawMoney(address payable _to, uint _amount) public {
+        require(_amount <= balanceReceived[msg.sender], "not enough funds");
+        balanceReceived[msg.sender] -= _amount;
+        _to.transfer(_amount);
+    }
+
+    function withdrawAllMoney(address payable _to) public {
+        uint balanceToSend = balanceReceived[msg.sender];
+        balanceReceived[msg.sender] = 0;
+        _to.transfer(balanceToSend);
+    }
+}
